98ec19e423826298a3c66b65cd7e365b3f30fb2a
ghostanza <friedcaligari@gmail.com>
2017-09-29 18:38:18 -0400

    multiple updates

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,6 @@
-require('dotenv').config();
-
 var express = require('express'),
     fs = require('fs'),
     path = require('path'),
-    morgan = require('morgan'),
     chalk = require('chalk'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
@@ -18,22 +15,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 
-// use morgan to log requests
-//app.use(morgan(':remote-addr - :remote-user [:date[web]] ":method :url HTTP/:http-version" :status ":user-agent"', {stream: accessLog}));
-
 // use the router
 app.use(routes);
 
 // 404 handler
 app.use((req, res, next) => {
-  //logger.log_404(req);
   res.status(404);
   res.redirect('/');
 });
 
 // 500 handler
 app.use((err, req, res, next) => {
-  //logger.log_500(err, req);
   res.send("500 error");
 });
 
